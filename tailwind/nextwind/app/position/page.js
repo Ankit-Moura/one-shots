340ca44e7f4646eeec85477865d1f049937fbe7a
WiseVizo <mecoc1011@gmail.com>
2025-02-16 10:50:51 +0530

    positions in css:

--- tailwind/nextwind/app/position/page.js
+++ tailwind/nextwind/app/position/page.js
@@ -2,7 +2,9 @@ export default function page(){
     return <div className="text-gray-300">
     <h2 className="">Css positioning</h2>
     <ul className="ml-2">
-        <li>static</li>
+        <li>static : Default css positioning for all elements
+            <p>does not overlap with other elements unless forced to using margin, flex etc,</p>
+        </li>
         <li>relative</li>
         <li>absolute</li>
         <li>fixed</li>
